44036bb8c1736248f50bb62a8a6b1583e4ab7d15
Berkin Bulut <48878104+bekX0@users.noreply.github.com>
2025-07-16 21:02:45 +0300

    initial commit

---
+++ src/main/java/com/bekx/studenttestservice/StudentTestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bekx.studenttestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentTestServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bekx/studenttestservice/StudentTestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bekx.studenttestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
